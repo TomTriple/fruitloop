e30a1ae0310c7a72488d5e87a431ec03e325ae07
thoefer <mail@tomhoefer.de>
2011-12-26 20:06:38 +0100

    code gen works partially

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -1,33 +1,4 @@
-class Token; end; 
-
-class TIdentifier < Token
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-end
-
-class TNumber  < Token
-  attr_accessor :number 
-  def initialize(number)
-    @number = number
-  end
-end
-
-class TBinOp < Token
-  attr_accessor :op
-  def initialize(op)
-    @op = op
-  end
-end
-
-class TColon < Token; end;
-class TEq < Token; end;
-class TTerminate < Token; end;
-class TEnd < Token; end;
-class TLoop < Token; end;
-class TDo < Token; end;
-class TSemicolon < Token; end;
+require "token/all"
 
 
 class Lexer
